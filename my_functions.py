83ee2d2c29376e61d17fcabef105b049a9b6ab1e
heyscottn <heyscottn@gmail.com>
2024-02-12 12:51:57 -0600

    mm

--- my_functions.py
+++ my_functions.py
@@ -1,30 +1,31 @@
-import json, requests
+import json
+from pip._vendor import requests
 
 # ---- functions for API ----
 def get_dad_joke():  
-  url = 'https://icanhazdadjoke.com/'
-  heads = {'Accept': 'application/json'}
-  response = requests.get(url, headers=heads).json()
-  return response
+    url = 'https://icanhazdadjoke.com/'
+    heads = {'Accept': 'application/json'}
+    response = requests.get(url, headers=heads).json()
+    return response
 
 # reveive a list and a string
 # search for a string
 # return a list
 def make_sublist(lst, word):
-  pass
+    pass
 
 
 # ---- functions to get data from a text file
 # reads from a file
 # returns one string
 def get_one_string(filename):
-  pass
+    pass
 
 
 # reads from a file
 # returns a list of floats
 def get_float_list(filename):
-  pass
+    pass
 
 
 
@@ -35,14 +36,14 @@ def get_float_list(filename):
 # removes newlines
 # returns a string
 def clean_string(s, drop_list):
-  pass
+    pass
 
 
 
 # receives a string, splits to list of strings
 # returns a dictionary with key:value = item:count
 def create_counts_dict(s):
-  pass
+    pass
 
 
 
